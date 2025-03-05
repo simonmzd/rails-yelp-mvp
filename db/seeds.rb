e032a9e82a9edd4d59af57ff7a8cfeddcb25e624
sm <mazaudsimon@gmail.com>
2025-03-05 15:50:50 +0100

    done restaurant part

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,13 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+5.times do
+  restaurant = Restaurant.new(
+    name: Faker::TvShows::BigBangTheory.character,
+    address: Faker::Address.full_address,
+    phone_number: Faker::PhoneNumber.phone_number,
+    category: ["chinese", "italian", "japanese", "french", "belgian"].sample
+  )
+  restaurant.save
+end
